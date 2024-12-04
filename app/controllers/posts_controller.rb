0aa83e80c068692d000fc10fa6b53c359a5f2c95
A0ipnks <n.blue10222201@gmail.com>
2024-12-04 23:17:22 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,77 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[ show edit update destroy ]
+
+  # GET /posts or /posts.json
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/1 or /posts/1.json
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts or /posts.json
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: "投稿完了" }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1 or /posts/1.json
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: "投稿を更新しました" }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1 or /posts/1.json
+  def destroy
+    @post.destroy!
+    
+    respond_to do |format|
+      format.html { redirect_to posts_path, status: :see_other, notice: "投稿が削除されました" }
+      format.json { head :no_content }
+    end
+  end
+
+  # すべての投稿を削除するアクション（Destroy All）
+  def destroy_all
+    Post.destroy_all
+    redirect_to posts_url, notice: 'すべての投稿が削除されました。'
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.require(:post).permit(:title, :memo)
+    end
+end
